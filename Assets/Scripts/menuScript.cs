af854a8da3e32adc00865d063a01c65e5e9b33f0
pakarnat <pakarinen.kimmo@gmail.com>
2017-10-17 15:59:16 +0300

    Menu & organisointia
    Placeholder päävalikko, sceneille oma kansio.

---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+
+
+	// Use this for initialization
+	void Start () {
+
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;
+		
+	}
+	
+	public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void StarLevel()
+    {        
+        SceneManager.LoadScene("playscene");
+        
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
