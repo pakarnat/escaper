be44a772455ef800a7b6f4d53a850660194d7d37
pakarnat <pakarinen.kimmo@gmail.com>
2017-10-23 13:23:34 +0300

    First Iteration
    Simple Inventory

---
+++ Assets/Scripts/Items/InventoryDetails.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item/Inventory", fileName = "Inventory Data")]
+public class InventoryDetails : ScriptableObject {
+
+    public List<Item> InventoryItems;
+    [Range(5,50)]
+    public int TotalSlots;
+
+}
---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu(menuName:"GameItem")]
+[RequireComponent(typeof(Rigidbody))]
+public class Item : MonoBehaviour {
+
+    public string Name = "New Item";
+    [Multiline(3)]
+    public string Description = "Item Description";
+    public Sprite Sprite;
+    public GameObject GameObjectPrefab;
+}
