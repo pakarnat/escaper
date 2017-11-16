cb3a536ba728e286930e83e0d21bb191257e1783
severi <severikupari1@gmail.com>
2017-11-16 14:13:24 +0200

    Ny ois valmis

---
+++ Assets/Scripts/Items/doorControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorControl : MonoBehaviour {
+
+
+    public float speed = 3;
+    public float angle = 0;
+    public Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+        angle = transform.eulerAngles.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Mathf.Round(transform.eulerAngles.y) != angle)
+        {
+            transform.Rotate(direction * speed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            angle = 90;
+            direction = Vector3.up;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            angle = 0;
+            direction = -Vector3.up;
+        }
+    }
+}
