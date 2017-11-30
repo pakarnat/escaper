9c7501233a38454d354f94cc1e2061564f114bac
severi <severikupari1@gmail.com>
2017-11-30 15:34:27 +0200

    muutoksia-open-jeesillä

--- Assets/walldoor.cs
+++ Assets/walldoor.cs
@@ -9,7 +9,7 @@ public class walldoor : MonoBehaviour {
 
     public bool open = false;
     //public bool close =true;
-
+    public Transform target;
 
 
     void Update()
@@ -40,20 +40,27 @@ public class walldoor : MonoBehaviour {
         }
 
 
-
+        Quaternion rot;
         if (open)
         {
             //var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, 45.0f, 0.0f), Time.deltaTime * 200);
-            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, 45.0f, 0.0f), Time.deltaTime * 200);
-            transform.rotation = newRot;
+            var tr = target.rotation.eulerAngles + new Vector3(0.0f, 45.0f, 0.0f);
+            var q =  Quaternion.Euler(tr);
+            //Debug.Log("open: " + q);
+            rot = Quaternion.RotateTowards(transform.rotation, q, Time.deltaTime * 200);
         }
         else
         {
 
 
             // y on -5 koska ei mennyt suoraan 0 vaan meni kohti 5. 
-            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, -5.0f, 0.0f), Time.deltaTime * 200 );
-            transform.rotation = newRot;
+            // var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, -5.0f, 0.0f), Time.deltaTime * 200 );
+            //var q = Quaternion.Euler(0.0f, -5.0f, 0.0f);
+            //Debug.Log("close: " + q);
+            rot = Quaternion.RotateTowards(transform.rotation, target.rotation, Time.deltaTime * 200);
+            //rot = Quaternion.RotateTowards(transform.rotation, new Quaternion(0,0,0,0), Time.deltaTime * 200);
+            //transform.rotation = newRot;
         }
+        transform.rotation = rot;
     }
 }
