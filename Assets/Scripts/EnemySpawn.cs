40399a27274e61f4a549f30676ddd5da967eb045
xCldx <32733997+xCldx@users.noreply.github.com>
2017-12-11 18:11:42 +0200

    SpwanPoint + sun +bug fix water
    Spawnpoint script newly written sunisgoingdown and fixed terrain on lake

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -6,34 +6,33 @@ public class EnemySpawn : MonoBehaviour
 {
 
     public GameObject enemy;                // The enemy prefab to be spawned.
-    public float spawnTime = 0.05f;            // How long between each spawn.
+    public float spawnTime = 1f;            // How long between each spawn.
     public Transform spawnPoints;         // An array of the spawn points this enemy can spawn from.
-    
-    
 
-
-
-        void Start()
-        {
-            // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
-           
-
-        }
     private void OnTriggerEnter(Collider other)
     {
-       InvokeRepeating("Spawn", spawnTime, spawnTime);
-    }
-         
-    void Spawn()
+        if (other.gameObject.tag == "Player")
         {
-            float spawnPointX = Random.Range(-500, 500);
-            float spawnPointY = Random.Range(-100, 100);
-            float spawnPointZ = Random.Range(-1000, 2000);
-            Vector3 spawnPosition = new Vector3(spawnPointX + spawnPoints.transform.position.x, spawnPointY + spawnPoints.transform.position.y, spawnPointZ + spawnPoints.transform.position.z);
-            // Find a random index between zero and one less than the number of spawn points.
-
-            Instantiate(enemy, spawnPosition, enemy.transform.rotation);
+            InvokeRepeating("Spawn", spawnTime, spawnTime);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            CancelInvoke("Spawn");
+        }
+    }
+
+    void Spawn()
+    {
+            float spawnPointX = Random.Range(-500, 500);
+            float spawnPointY = Random.Range(50, 300);
+            float spawnPointZ = Random.Range(-500, 500);
+            Vector3 spawnPosition = new Vector3(spawnPointX + spawnPoints.transform.position.x, spawnPointY + spawnPoints.transform.position.y, spawnPointZ + spawnPoints.transform.position.z);
+            Instantiate(enemy, spawnPosition, enemy.transform.rotation);
+    }
     
 }
 
---
+++ Assets/Scripts/SunIsGoingDown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunIsGoingDown : MonoBehaviour {
+
+    public Light sun;
+    public float brightness = 1.2f;
+    public float brightnessTicks = 0.005f;
+    public float timeOfTicks = 1f;
+    public float ticksStartingTime = 5f;
+    public float darknessPower = 0.3f;
+
+
+	// Use this for initialization
+	void Start () {
+        sun.intensity = brightness;
+        darkness();
+	}
+
+    private void darkness()
+    {
+        if (sun.intensity > (darknessPower + brightnessTicks))
+        {
+            sun.intensity += -(brightnessTicks);
+            InvokeRepeating("darkness", ticksStartingTime, timeOfTicks);
+        }
+    }
+}
