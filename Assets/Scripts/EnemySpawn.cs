c2d8ee48022d37d6361f6994a221b64204854020
xCldx <32733997+xCldx@users.noreply.github.com>
2017-12-13 14:16:57 +0200

    epileptikot kiittää
    =)

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -6,10 +6,11 @@ public class EnemySpawn : MonoBehaviour
 {
 
     public GameObject enemy;                // Vihollisen Prefab
-    public float spawnTime = 1f;            // Mitenkä pitkä väli vihollisien spawnien välissä.
+    public float spawnTime = 5f;            // Mitenkä pitkä väli vihollisien spawnien välissä.
     public Transform spawnPoints;         // Mistä kohteesta viholliset spawnaavat.
     AudioSource thunder;
-    public static int killedEnemies = 0;    
+    public static int killedEnemies = 0;
+    public Light sun;
 
     void Start()
     {
@@ -22,8 +23,10 @@ public class EnemySpawn : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+          
             thunder.Play();
             InvokeRepeating("Spawn", spawnTime, spawnTime);
+         
         }
     }
     //kun pelaa poistuu alueelta niin scripti lopettaa Spawn funktion kutsun.
@@ -31,18 +34,31 @@ public class EnemySpawn : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            CancelInvoke("Spawn");           
+            CancelInvoke("Spawn");
+            CancelInvoke("valopaalle");
+            CancelInvoke("valopois");
         }
+    }
+    void valopaalle()
+    {
+        sun.intensity = 50f;
+    }
+    void valopois()
+    {
+        sun.intensity = 0.3f;
     }
 
     void Spawn()
     {
+        InvokeRepeating("valopaalle", 0, spawnTime);
+        InvokeRepeating("valopois", 0.05f, spawnTime);
         float spawnPointX = Random.Range(-500, 500); // random x arvo
         float spawnPointY = Random.Range(50, 300); // random y arvo
         float spawnPointZ = Random.Range(-500, 500); //random z arvo
                                                      //spawnPosition arvo on spawnPoints sen hetkinen arvo plus sattuman varainen arvon lisäys yläpuolella olevien random muuttujien arvon mukaan
         Vector3 spawnPosition = new Vector3(spawnPointX + spawnPoints.transform.position.x, spawnPointY + spawnPoints.transform.position.y, spawnPointZ + spawnPoints.transform.position.z);
-        Instantiate(enemy, spawnPosition, enemy.transform.rotation); // spawnataan uusi vihollinen spawnPosition arvoon        
+        Instantiate(enemy, spawnPosition, enemy.transform.rotation); // spawnataan uusi vihollinen spawnPosition arvoon   
+        
     }  
     
 }
