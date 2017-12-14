400cd265636f310a84cde0b754701f2db85bae4a
xCldx <32733997+xCldx@users.noreply.github.com>
2017-12-14 17:21:52 +0200

    minimap
    minimap

---
+++ Assets/Scripts/minimap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minimap : MonoBehaviour {
+
+
+    public List<Light> Lights;
+
+    void OnPreCull()
+    {
+        foreach (Light light in Lights)
+        {
+            light.enabled = false;
+        }
+    }
+
+    void OnPostRender()
+    {
+        foreach (Light light in Lights)
+        {
+            light.enabled = true;
+        }
+    }
+
+}
+
